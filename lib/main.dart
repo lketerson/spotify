01366a4adf62e7f0beb44c0385bd1518cd12e795
lketerson <lucasketerson@gmail.com>
2025-02-09 15:42:37 -0300

    Modificado: main home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/presentation/splash/pages/splash.dart';
+
+import 'core/configs/themes/app_theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +12,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      home: const SplashPage(),
     );
   }
 }
