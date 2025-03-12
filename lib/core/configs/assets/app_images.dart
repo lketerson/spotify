06e30cb4713516fa76d9aca65dc5e8fb0a3c8a4f
lketerson <lucasketerson@gmail.com>
2025-03-11 21:33:12 -0300

    Modificado: adicionado nova rota de imagem

--- lib/core/configs/assets/app_images.dart
+++ lib/core/configs/assets/app_images.dart
@@ -3,4 +3,5 @@ class AppImages {
   static const format = '.png';
   static const introBg = "${basePath}intro_bg$format";
   static const chooseModeBg = "${basePath}choose_mode_bg$format";
+  static const authBg = "${basePath}auth_bg$format";
 }
